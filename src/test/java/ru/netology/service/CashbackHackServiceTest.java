fbd94e2b5a81287b3e4bf3ddf55c83ff060bf538
Liza Stepanova <tuchafeb@mail.ru>
2023-06-19 23:39:02 +0300

    git commit -m "build. gradle"

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,57 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
-  
+    @Test
+    public void testRemainForCashbackIfPurchaseLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+
+        int expected = 100;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testRemainForCashbackIfPurchase1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testRemainForCashbackIfPurchaseMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int actual = service.remain(amount);
+
+        int expected = 900;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testRemainIfPurchaseZero() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+
+    }
+
+
 }
